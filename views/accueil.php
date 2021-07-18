58211656be2716c356874a525b00051629f3af48
cedricgallet <galletcedric@protonmail.com>
2021-07-18 21:47:12 +0200

    update

--- views/accueil.php
+++ views/accueil.php
@@ -38,11 +38,18 @@
                 <div class="card bg-light shadow bg-body rounded-3 mb-2">
                     <div class="card-body">
                         <h2 class="card-title">
-                            TITRE
+                            Qu'est-ce qu'une faille XSS ?
                         </h2>
-                        <p class="card-text">BLaaaaaaaaaaablaaaaaaaaaaaaaaaaa
+                        <p class="card-text">Une faille XSS consiste à injecter du code directement interprétable par le
+                            navigateur Web, comme, par exemple, du JavaScript ou du HTML. Cette attaque ne vise pas
+                            directement le site comme le ferait une injection SQL mais concerne plutôt la partie client
+                            c'est-à-dire vous (ou plutôt votre navigateur). Ce dernier ne fera aucune différence entre
+                            le code du site et celui injecté par le pirate, il va donc l'exécuter sans broncher. Les
+                            possibilités sont nombreuses : redirection vers un autre site, vol de cookies, modification
+                            du code HTML de la page, exécution d'exploits contre le navigateur : en bref, tout ce que
+                            ces langages de script vous permettent de faire.
                         </p>
-                        <div class="d-flex text-center border-top border-1 pt-2">Continuer à lire l'article
+                        <div class="d-flex text-center border-top border-1 pt-2">Comment s'en protéger?
 
                         </div>
                     </div>
@@ -51,11 +58,19 @@
                 <div class="card bg-light shadow bg-body rounded-3 mb-2">
                     <div class="card-body">
                         <h2 class="card-title text-dark">
-                            TITRE
+                            Qu'est-ce qu'une attaque MITM 
                         </h2>
-                        <p class="card-text">BLaaaaaaaaaaablaaaaaaaaaaaaaaaaa
+                        <p class="card-text">L'attaque de l'homme du milieu (HDM) ou man-in-the-middle attack (MITM),
+                            parfois appelée attaque de l'intercepteur, est une attaque qui a pour but d'intercepter les
+                            communications entre deux parties, sans que ni l'une ni l'autre ne puisse se douter que le
+                            canal de communication entre elles a été compromis. Le canal le plus courant est une
+                            connexion à Internet de l'internaute lambda. L'attaquant doit d'abord être capable
+                            d'observer et d'intercepter les messages d'une victime à l'autre. L'attaque « homme du
+                            milieu » est particulièrement applicable dans la méthode d'échange de clés Diffie-Hellman,
+                            quand cet échange est utilisé sans authentification. Avec authentification, Diffie-Hellman
+                            est en revanche invulnérable aux écoutes du canal, et est d'ailleurs conçu pour cela.
                         </p>
-                        <div class="d-flex text-center border-top border-1 pt-2">Continuer à lire l'article
+                        <div class="d-flex text-center border-top border-1 pt-2">Comment s'en protéger?
 
                         </div>
                     </div>
@@ -64,7 +79,8 @@
                 <!-- =========================AJOUTER UN ARTICLE========================== -->
                 <div id="space" class="card rounded-3 mb-2 text-center">
                     <div class="card-body"><a href="/controllers/userConnexion-controllers.php"><button type="submit"
-                                class="btn btn-outline-warning w-30">Inscris-toi et bénificie des Solutions!!</button></a>
+                                class="btn btn-outline-warning w-30">Inscris-toi et bénificie des
+                                Solutions!!</button></a>
                     </div>
                 </div>
                 <!-- ========================================================================== -->
@@ -76,7 +92,7 @@
                         </h2>
                         <p class="card-text">BLaaaaaaaaaaablaaaaaaaaaaaaaaaaa
                         </p>
-                        <div class="d-flex text-center border-top border-1 pt-2">Continuer à lire l'article
+                        <div class="d-flex text-center border-top border-1 pt-2">Comment s'en protéger?
 
                         </div>
                     </div>
@@ -89,7 +105,7 @@
                         </h2>
                         <p class="card-text">BLaaaaaaaaaaablaaaaaaaaaaaaaaaaa
                         </p>
-                        <div class="d-flex text-center border-top border-1 pt-2">Continuer à lire l'article
+                        <div class="d-flex text-center border-top border-1 pt-2">Comment s'en protéger?
 
                         </div>
                     </div>
@@ -98,7 +114,8 @@
                 <!-- =========================AJOUTER UN ARTICLE========================== -->
                 <div id="space" class="card rounded-3 mb-2 text-center">
                     <div class="card-body"><a href="/controllers/userConnexion-controllers.php"><button type="submit"
-                                class="btn btn-outline-warning w-30">Inscris-toi et bénificie des Solutions!!</button></a>
+                                class="btn btn-outline-warning w-30">Inscris-toi et bénificie des
+                                Solutions!!</button></a>
                     </div>
                 </div>
                 <!-- ========================================================================== -->
@@ -111,7 +128,7 @@
                             TITRE
                         </h2>
                         <p class="card-text">BLaaaaaaaaaaablaaaaaaaaaaaaaaaaa
-                            <div class="d-flex text-center border-top border-1 pt-2">Continuer à lire l'article
+                            <div class="d-flex text-center border-top border-1 pt-2">Comment s'en protéger?
 
                             </div>
                     </div>
@@ -124,8 +141,7 @@
                         </h2>
                         <p class="card-text">BLaaaaaaaaaaablaaaaaaaaaaaaaaaaa
                         </p>
-                        <div class="d-flex text-center border-top border-1 pt-2">Continuer à lire l'article
-
+                        <div class="d-flex text-center border-top border-1 pt-2">Comment s'en protéger?
                         </div>
                     </div>
                 </div>
--- views/faille-web.php
+++ views/faille-web.php
@@ -3,8 +3,44 @@
 // Génération du Header+Navbar:
 include(dirname(__FILE__).'/../views/templates/navBar.php');
 
-// Génération du Main:
-include(dirname(__FILE__).'/accueil.php');
+?>
+
+<div class="card bg-light shadow bg-body rounded-3 mb-2">
+    <div class="card-body">
+        <h2 class="card-title">
+            S'en protéger ?
+        </h2>
+        <p class="card-text">
+
+            XSS est l'abréviation de Cross-Site Scripting.
+
+            Pour l'anecdote, l'abréviation aurait dû être logiquement CSS mais comme ce terme était déjà employé pour
+            désigner les feuilles de styles (Cascading Style Sheets en anglais), le X fut choisi en raison du premier
+            mot (cross ou croix en français).
+
+            Pour se protéger des XSS il faut remplacer les caractères qui pourraient éventuellement
+            être compris par le navigateur comme des balises par leur entité HTML.
+
+            Le navigateur affichera textuellement le caractère et ne cherchera plus à l'interpréter.
+
+
+            htmlentities encode tous les caractères spéciaux mais aussi les é è à ù… alors que htmlspecialchars
+            se contente des caractères spéciaux ce qui suppose donc que vous utilisez un charset supportant les
+            caractères comme é è à ù sinon vous aurez probablement des � à la place.
+
+            Le paramètre ENT_QUOTES est ajouté à la fonction htmlentities ou htmlspecialchars pour préciser
+            d'échapper également les simples guillemets car cela peut être problématique, notamment si, par exemple,
+            vous utilisez la chaîne vulnérable dans un attribut d'une balise HTML qui est sous la forme
+            attribut='valeur' : le simple guillemet n'étant pas échappé, il est donc encore possible de fermer cet
+            attribut. Le ENT_QUOTES empêchera cela.
+
+        </p>
+    </div>
+</div>
+
+<?php
 
 // Génération du Footer:
 include(dirname(__FILE__).'/../views/templates/footer.php');
+
+?>
