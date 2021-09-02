3247b87ec586475aa263e6f6635540cd979fb0c5
cedricgallet <galletcedric@protonmail.com>
2021-09-02 14:03:04 +0200

    update

--- views/form/landingAddAvatar.php
+++ views/form/landingAddAvatar.php
@@ -7,10 +7,10 @@ require_once __DIR__.'/../../views/templates/navbar.php';
     <div class="row justify-content-center h-100">
         <h2 class="d-flex justify-content-center align-items-center">Modifier mon avatar</h2>
 
-        <div class="col-lg-4 p-0">
+        <div class="col-lg-5 p-0">
             <form action="layouts/change_avatar.php" method="POST" enctype="multipart/form-data">
                 <div class="mb-3">
-                    <label for="avatar" class="text-warning">Images autorisées : png, jpg, jpeg, gif - max 20Mo</label>
+                    <label for="avatar" class="text-warning">Images autorisées : png, jpg, jpeg, gif - 20Mo Max</label>
                     <input type="file" 
                             name="avatar_file"
                             accept="image/png, image/jpg, image/jpeg, image/gif"
--- views/landing.php
+++ views/landing.php
@@ -6,12 +6,12 @@ require_once __DIR__.'/../views/templates/navbar.php';
     <div class="row h-100">
         <div class="col-12 col-lg-12">
             <div class="text-center h-100">
-                <div><h2 class="p-5">Bonjour <?=$_SESSION['user']['pseudo'] ?> ! </h2></div>
-                <div class="mb-4"><h2><?=$title ?? ''?></h2></div>
+                <div><h2 class="pt-5 mb-3">Bonjour <?=$_SESSION['user']['pseudo'] ?> ! </h2></div>
+                <div class="mb-5"><h2><?=$title ?? ''?></h2></div>
             </div>
         </div>
         <!-- Boutton -->
-        <div class="col-12 col-lg-6 d-flex flex-column align-items-center mt-5 h-100">
+        <div class="col-12 col-lg-6 d-flex flex-column align-items-center mt-3 h-100">
             <a href="/../views/home.php" class="boutton btn btn-danger mb-2">Accueil</a>
             <a href="/../views/form/landingUpdatePassword.php" class="boutton btn btn-info text-white mb-2">Changer
                 mon mot de passe</a>
@@ -21,15 +21,15 @@ require_once __DIR__.'/../views/templates/navbar.php';
                 <a href="/../controllers/delete-ctrl.php" class="boutton btn btn-warning ">supprimer mon compte</a>
         </div>
         
-        <div class="col-12 col-lg-6 d-flex justify-content-center mt-5 h-100">
-            <div class="card" style="width: 20rem; height:30rem;">
+        <div class="col-12 col-lg-6 d-flex justify-content-center mt-3 h-100">
+            <div class="card" style="width: 20rem; height:31rem;">
                 <img src="/../assets/img/membres/avatars/avatar1.jpeg" class="card-img-top" alt="avatar-personnel">
-                <div class="card-body shadow">
-                    <h4 class="card-title">Mes informations</h4>
+                <div class="card-body">
+                    <h4 class="card-title mb-0">Mes informations</h4>
                     <ul class="list-group list-group-flush">
-                        <li class="text-info">Pseudo: <?=$_SESSION['user']['pseudo'] ?></li>
-                        <li class="text-info">Email: <?=$_SESSION['user']['email'] ?></li>
-                        <li class="text-info">IP: <?=$_SESSION['user']['ip'] ?></li>
+                        <li class="list-group-item text-info">Pseudo: <?=$_SESSION['user']['pseudo'] ?></li>
+                        <li class="list-group-item text-info">Email: <?=$_SESSION['user']['email'] ?></li>
+                        <li class="list-group-item text-info pb-0 pt-0">IP: <?=$_SESSION['user']['ip'] ?></li>
                     </ul>   
                 </div>
             </div>
