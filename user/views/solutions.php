fd7009ed79908874d7eb0009e0a60a073b96f32e
cedricgallet <galletcedric@protonmail.com>
2021-11-27 17:26:55 +0100

    update

--- user/views/solutions.php
+++ user/views/solutions.php
@@ -1,7 +1,7 @@
 <!-- ++++++++++++++++++++++++++++++Solutions++++++++++++++++++++++++++++++++ -->
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-10 col-md-12 col-sm-12">
+            <div class="col-lg-10 col-md-12 col-sm-12">BONJOUR !
                 <!-- ===============================CONTENU DES SOLUTIONS======================================= -->
             </div>
             <?php require_once dirname(__FILE__).'/../views/templates/rightMenu.php';?>
